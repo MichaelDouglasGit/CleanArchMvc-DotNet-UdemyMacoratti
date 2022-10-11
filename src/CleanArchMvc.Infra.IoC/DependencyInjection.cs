260c44ee80242dcc375d2b96df870235bc9e484e
Michael Douglas <michael.douglas.amarante@gmail.com>
2022-10-11 16:49:45 -0300

    criação das index views

--- src/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ src/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -4,6 +4,9 @@ using CleanArchMvc.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context.Repositories;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
 
 namespace CleanArchMvc.Infra.IoC
 {
@@ -12,10 +15,16 @@ namespace CleanArchMvc.Infra.IoC
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), 
                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
             return services;            
         }
